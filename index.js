eb0f38ecc312d31c0e468ac5f536bade0bd63bc7
vidit <vidit.mittal02@gmail.com>
2022-01-15 15:02:13 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,66 @@
+
+var noButtons = document.querySelectorAll(".drum").length;
+for (var i = 0; i < noButtons; i++)
+{
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+  function handleClick()
+  {
+    var button= this.innerHTML;
+    makesound(button);
+    buttonAnimation(button);
+  }
+}
+document.addEventListener("keydown",handlekey);
+function handlekey(){
+  makesound(event.key);
+  buttonAnimation(event.key);
+}
+
+
+function makesound(key)
+{
+  switch (key)
+  {
+    case "w" :
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+      break;
+    case "a":
+            var kick= new Audio("sounds/kick-bass.mp3");
+            kick.play();
+      break;
+
+    case "s":
+            var snare= new Audio("sounds/snare.mp3");
+            snare.play();
+      break;
+    case "d":
+            var tom1=new Audio("sounds/tom-1.mp3") ;
+            tom1.play();
+      break;
+    case "j":
+            var tom2= new Audio("sounds/tom-2.mp3") ;
+              tom2.play();
+      break;
+    case "k":
+            var tom3= new Audio("sounds/tom-3.mp3") ;
+            tom3.play();
+      break;
+    case "l":
+            var tom4= new Audio("sounds/tom-4.mp3") ;
+            tom4.play();
+      break;
+    default:
+      console.log(button);
+  };
+
+
+}
+function buttonAnimation(currentkey)
+{
+  var buttonclass= document.querySelector("."+ currentkey);
+  buttonclass.classList.add("pressed");
+  setTimeout(function(){buttonclass.classList.remove("pressed")},100);
+
+}
